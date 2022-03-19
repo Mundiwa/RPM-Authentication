74ccdeea71cb3ce4c654e72b6c57e9d790018bf0
Mundiwa <98858398+Mundiwa@users.noreply.github.com>
2022-03-19 06:32:36 +0200

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.rpm_authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class MainActivity extends AppCompatActivity {
+    int splashTime = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(()-> startActivity(new Intent(getApplicationContext(), Login.class)),splashTime);
+    }
+}
